3b81a305b94838ac4c6317038e484c3f988f7e01
Srimanta Bag <srimantabag2005@gmail.com>
2026-01-20 21:18:39 +0530

    Updated files

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9n12$kyba1io+5o_16-m+a^ic9*032%w%qi82fj*27h#wf%k9#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'event-countdown-timer-9eki.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
